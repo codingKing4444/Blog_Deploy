2c1b348eb33b36f0d9a5b5a4a6801c53be679ba1
codingKing4444 <149951770+codingKing4444@users.noreply.github.com>
2024-04-13 01:38:05 +0530

    Update OAuth.jsx

--- Frontend/src/components/OAuth.jsx
+++ Frontend/src/components/OAuth.jsx
@@ -17,7 +17,7 @@ export default function OAuth()  {
         try{
           const resultFromGoogle = await signInWithPopup(auth, provider)
           // console.log(resultFromGoogle);
-          const res = await fetch('/api/auth/google',{
+          const res = await fetch('https://blog-deploy-api.vercel.app/api/auth/google',{
             method:'POST',
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify({
