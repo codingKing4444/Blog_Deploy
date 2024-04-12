9c7d3fce2e147c78c8a25331c2a1ef8f6a499412
codingKing4444 <149951770+codingKing4444@users.noreply.github.com>
2024-04-13 01:33:36 +0530

    Update DashProfile.jsx

--- Frontend/src/components/DashProfile.jsx
+++ Frontend/src/components/DashProfile.jsx
@@ -100,7 +100,7 @@ export default function DashProfile() {
 
     try {
       dispatch(updateStart());
-      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+      const res = await fetch(`https://blog-deploy-api.vercel.app/api/user/update/${currentUser._id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -125,7 +125,7 @@ export default function DashProfile() {
     setShowModal(false);
     try {
       dispatch(deleteUserStart());
-      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+      const res = await fetch(`https://blog-deploy-api.vercel.app/api/user/delete/${currentUser._id}`, {
         method: 'DELETE',
       });
       const data = await res.json();
@@ -141,7 +141,7 @@ export default function DashProfile() {
 
   const handleSignout = async () => {
     try {
-      const res = await fetch('/api/user/signout', {
+      const res = await fetch('https://blog-deploy-api.vercel.app/api/user/signout', {
         method: 'POST',
       });
       const data = await res.json();
