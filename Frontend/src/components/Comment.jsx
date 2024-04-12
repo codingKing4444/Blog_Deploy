040506ce3eb0ffba2ced1656f7824bb2134dc101
codingKing4444 <149951770+codingKing4444@users.noreply.github.com>
2024-04-13 01:30:36 +0530

    Update Comment.jsx

--- Frontend/src/components/Comment.jsx
+++ Frontend/src/components/Comment.jsx
@@ -13,7 +13,7 @@ export default function Comment({ comment, onLike, onEdit, onDelete }) {
   useEffect(() => {
     const getUser = async () => {
       try {
-        const res = await fetch(`/api/user/${comment.userId}`);
+        const res = await fetch(`https://blog-deploy-api.vercel.app/api/user/${comment.userId}`);
         const data = await res.json();
         if (res.ok) {
           setUser(data);
@@ -33,7 +33,7 @@ export default function Comment({ comment, onLike, onEdit, onDelete }) {
   // 9hr 36 min
   const handleSave = async () => {
     try {
-      const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+      const res = await fetch(`https://blog-deploy-api.vercel.app/api/comment/editComment/${comment._id}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
